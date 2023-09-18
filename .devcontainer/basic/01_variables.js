4cccd26af8996e0a2a07aadd12c0d4e0c425b01c
Goutam gurjar <123896240+goutamgurjar09@users.noreply.github.com>
2023-09-18 10:28:49 +0000

    container config done

---
+++ .devcontainer/basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345;
+//accountId = 12; not allowed
+//console.log(accountId);
+var email = "g@gmail.com";
+email = "k@gmail.com"
+accountCity = "jaipur";
+//accountCity = "bangluru"
+//console.log(email);
+
+let password = 3953;
+password = 1375;
+console.log(password);
+let name;  // undefined   
+console.table([password,email,accountId,accountCity,name]);
+
+//pls prefer use let not var bcz the problem are block and function scope
