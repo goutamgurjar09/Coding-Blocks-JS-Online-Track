f3152c2750882fda7e47c48545035156ba47f6e7
Goutam gurjar <123896240+goutamgurjar09@users.noreply.github.com>
2023-09-20 17:47:06 +0000

    operation and prifix /postfix

---
+++ .devcontainer/basic/04_operation.js
@@ -0,0 +1,46 @@
+// let val = 3
+// let negval = -val;
+// console.log(negval);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%5)
+
+let str = "goutam"
+let str1 = " ranvey"
+//console.log(str + str1)
+
+// console.log("1" + 2);//12
+// console.log("1" + "2");//12
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32
+
+// console.log(+true);//1
+// console.log(+"");//0
+
+let n1 , n2 , n3
+n1 = n2 = n3 = 2 + 2 //4
+//console.log(n1);
+
+let getCounter = 100
+//getCounter++
+++getCounter
+console.log(getCounter);
+
+//in postfix first value add then increament
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+// let x = 3
+// let y = x++;
+//console.log(`x:${x}, y:${y}`)//x=4, y=3  
+let x = 3
+let y = ++x;
+console.log(`x:${x}, y:${y}`)//x=4, y=4
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
