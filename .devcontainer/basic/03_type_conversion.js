1f7c75f714c134b385a1c0d97b4feb038fa2c002
Goutam gurjar <123896240+goutamgurjar09@users.noreply.github.com>
2023-09-20 16:53:27 +0000

    conversion of datatype

---
+++ .devcontainer/basic/03_type_conversion.js
@@ -0,0 +1,38 @@
+//let score = 10;
+// console.log(typeof score);
+// score = "10";
+// score = "10abc";
+// score = null;
+// score = undefined;
+// score = true;
+// score = false;
+// score = "goutam";
+// console.log(typeof score);
+
+// let num = Number(score)//convert string to number
+// console.log(typeof num)
+// console.log(num)
+
+// "10" => 10
+// "10abc" => NaN
+//  true => 1; false => 0
+
+
+let isLogin = 1 //true
+//isLogin = 0
+//isLogin = "" //false
+//isLogin = "ranvey" //true
+
+//let booleanisLoggin = Boolean(isLogin)
+//console.log(booleanisLoggin);
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let somenum = 33
+let strnum = String(somenum)
+console.log(strnum);  //33
+console.log(typeof strnum); //string
+
+
+
