fe85500eedaaa6cc8e613b5587500f86bdb9f2eb
Goutam gurjar <goutamgurjar09@gmail.com>
2023-09-30 13:27:48 +0530

    variables

--- .devcontainer/basic/01_variables.js
+++ .devcontainer/basic/01_variables.js
@@ -1,17 +1,17 @@
-const accountId = 12345;
+//const accountId = 12345;
 //accountId = 12; not allowed
 //console.log(accountId);
-var email = "g@gmail.com";
-email = "k@gmail.com"
-accountCity = "jaipur";
+// var email = "g@gmail.com";
+// email = "k@gmail.com"
+// accountCity = "jaipur";
 //accountCity = "bangluru"
 //console.log(email);
 
-let password = 3953;
-password = 1375;
-console.log(password);
-let name;  // undefined   
-console.table([password,email,accountId,accountCity,name]);
+// let password = 3953;
+// password = 1375;
+// console.log(password);
+// let name;  // undefined   
+// console.table([password,email,accountId,accountCity,name]);
 
 //pls prefer use let not var bcz the problem are block and function scope
 
@@ -19,4 +19,51 @@ console.table([password,email,accountId,accountCity,name]);
 //node .devcontainer/basic/04_operation.js
 
 let num = 20;
-console.log(num);
+//console.log(num);
+//-------strings------
+//Way-1
+// let fName = 'goutam'
+// let lastName = 'gurjar'
+//  let space = ' '
+// console.log(fName,lastName,space);//goutam gurjar
+
+//Way-2
+// let fName = "goutam"
+// let lastName = "gurjar"
+// let space = " "
+// console.log(fName,space,lastName);//goutam gurjar
+
+//Way-3
+// let fName = `goutam`
+// let lastName = `gurjar`
+// let space = ` `
+// console.log(fName,space,lastName);//goutam gurjar
+
+// ------------------------
+// string concatination (concat) -> +
+//console.log(fName + lastName)
+//console.log(fName + space + lastName)
+// console.log(fName + ' ' + lastName)
+// console.log(fName + " " + lastName)
+// console.log(fName + ` ` + lastName)
+
+// ----------------------------------
+
+//let houseName = 'goutam gurjar's house'//you cant use single quote inside single quote
+//let houseName = 'goutam gurjar"s house'//it's work fine
+// let houseName = "goutam ranvey's house"//right
+// houseName = "goutam ranvey/s house"//right
+// houseName = "goutam ranvey/'s house"//right
+// console.log(houseName)
+
+//let barish = 'hogi\nNot'
+// let barish = "hogi\nNot"
+// let barish = "hogi" + "\n" + 'Not'
+//let barish = 'hogi + \n + Not'//hogi +
+                                //  + Not
+//let barish = 'hogi + "\n" + Not'//hogi + "
+                                //" + Not
+//let barish = 'hogi + '\n' + Not'//wrong
+//let barish = 'hogi , \n , Not'//hogi , 
+                             //, Not
+console.log(barish)
