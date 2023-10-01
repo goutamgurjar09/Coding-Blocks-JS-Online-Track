8b86e1e0e70b57780dfbd9adb9b5513c5b5417df
Goutam gurjar <goutamgurjar09@gmail.com>
2023-10-01 18:25:13 +0530

    js

---
+++ js/basic/more-aboutVariables.js
@@ -0,0 +1,99 @@
+
+
+// redeclare kar sakte hai? -> NO
+
+//  let something = 10;
+
+//  let something = 'sam';
+
+// -------------------------
+// reinitiailise kar sakte hai? -> YES  
+
+//   let something = 10;
+//   something = 'sam';
+//   console.log(something);
+
+// ---------------------------
+// js variables -> weakly typed 
+
+// let something = 100
+// console.log(typeof something);
+
+// something = "goutam"
+// console.log(typeof something);
+
+
+// something = true
+// console.log(typeof something);
+
+// -----------number--------------
+// let num  = 10;
+// let num2 = 10.5;
+// let num3 = -100
+// let s = 'goutam'
+// let s1 = "goutam"
+// let s2 = `gurjar`
+
+// console.log(typeof num);
+// console.log(typeof num2);
+// console.log(typeof num3);
+
+// console.log(typeof s1);
+// console.log(typeof s2);
+// console.log(typeof s3);
+
+// --------------------------------
+//let ans = s + s2
+// let ans = s - s2; //wrong NaN
+// let ans = s * s2; //wrong NaN
+// let ans = s / s2; //wrong NaN
+//console.log(ans);
+
+// --------------------------------
+//that's way Always remember mathematical operations 
+// -> + -> strings
+// -> + , - , * , / , %... etc -> numbers
+
+// let num  = 10;
+// let num2 = 10.5;
+// let num3  = -100;
+// let ans = num + num2 + num3;
+//  console.log(ans);
+
+// --------------------------------
+// let n  = 45;
+// let n1 = 35.5;
+//let ans = n - n1;
+//let ans = n % n1;
+//let ans = n / n1;
+ //console.log(ans);
+
+
+
+// --------------------------------
+//let n2 = 5;
+// let n2 = 4;
+// let n3 = 4;
+
+//let ans = n3%n2;
+//let ans = n2%n3;
+//console.log(ans)
+
+// ------------js PEMDAS rules like Bodmass--------------------
+// let ans = (22-4) * (22/2) + 4; // 18 * 11 + 4 -> 202
+// let ans2 = (12/4) / (22*2) - 14; // 3 / 44 - 14
+// let ans3 = 22-8 * (22/2) / 4;  // 22 - 8 * 11 / 4 -> 
+// // -> 22 - 8 * 2.75 -> false
+// -> 22 - 88 / 4 -> true 
+
+// console.log(ans)
+// console.log(ans2)
+// console.log(ans3)
+
+//----------without using datatype-----------------
+
+// g = 12;
+// g = "goutam";
+// g = true;
+// console.log(g)//12/goutam/true
+
