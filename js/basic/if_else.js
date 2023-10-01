6eb9bb7e2b5ec2ce6977938490ab1862e7b1981f
Goutam gurjar <goutamgurjar09@gmail.com>
2023-10-01 17:14:18 +0530

    js

---
+++ js/basic/if_else.js
@@ -0,0 +1,56 @@
+// if(true){
+//     console.log("swarg");
+// }
+
+// if(true)
+// console.log("me swarg me hu pario ke sath");//condition
+
+// console.log("pariya pankha chala rahi hai");//normal console.log flow
+
+
+// if(false)
+// console.log("me swarg me hu pario ke sath");//condition
+
+// console.log("pariya pankha chala rahi hai");//normal flow
+
+
+// --------------------------------
+// if(true){
+//     console.log("mai swarg mei hu, pariyo ke sath")
+// }
+// if(false){
+//     console.log("mai swarg mei hu, pariyo ke sath")//not execute
+// }
+// ----------------------------------
+//BLOCK -> {} -> SWARG KA DARWAZA -> (HEAVEN)
+
+// let score =33
+// score = 34
+// if(score>33){
+//     console.log("pappu pass ho gya");
+// }
+// else{
+//     console.log("pappu fail ho gya");
+// }
+
+// ---------------------------
+//block -> to bundle up
+// {
+//     console.log("aaja bete mauj krle")
+// }
+
+
+// ----------------------------
+
+// if, else if, else
+let score = 50
+if(score >= 90)
+console.log("A+");
+else if(score >= 80)
+console.log("B+");
+else if(score >= 70)
+console.log("c+");
+else if(score >= 60)
+console.log("c");
+else
+console.log("thoda sa to padh leta");
