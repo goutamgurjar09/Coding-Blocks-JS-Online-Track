d343bcf7244bece6241441b90e1b2f7c36046a79
Goutam gurjar <goutamgurjar09@gmail.com>
2023-10-31 23:36:34 +0530

    project

---
+++ js/basic/JS-Project/Notes-app/app.js
@@ -0,0 +1,18 @@
+let inp = document.querySelector('input')
+let btn =  document.querySelector('button')
+let list = document.querySelector('#list');
+
+btn.addEventListener('click',(e)=>{
+    let notetext =inp.value;
+    let li = document.createElement('li');
+    li.innerText = notetext;
+    list.appendChild(li);
+    inp.value = "";  //input text remove/khali ho jaay after append to li
+
+    //li pr click krne pr remove ho jaay and isko addevent. ke bahar nhi likh skte bcz iska scope yhi hai
+    li.addEventListener('click',function(e){
+        li.remove()
+        //e.target.remove();
+    })
+
+})
