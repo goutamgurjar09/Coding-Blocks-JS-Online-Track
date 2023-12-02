b2d2d164aee08db47957752f6eead5a7c21081f8
Goutam gurjar <goutamgurjar09@gmail.com>
2023-12-03 01:42:58 +0530

    js

--- js/basic/Timing Events/app.js
+++ js/basic/Timing Events/app.js
@@ -2,7 +2,7 @@
 //the window object allows us to execution of code at a specific time intervels this time intervelsare called timing events
 //the two key methods to use with js are-> setTimeout(),setInterval() and clearIntervel()
 
-//1. setTimeout() : methods sets a timer which executes a function or piece of code once the timer expires
+//1. setTimeout() :its a browser webApi   that sets a timer which executes a function or piece of code once the timer expires
 
 //2. setInterval() : methods, repeatedly calls a function or executes a code snippet, with a fixed time delay between each call
 //the setInterval() methodh will continue calling the function untill clearInterval() is called or the window is closed. the ID value rturned by
@@ -49,22 +49,23 @@ let startbtn = document.querySelector('#btn')
 
 //---------------------
 
-let stopbtn = document.querySelector('#btnTwo')
+// let stopbtn = document.querySelector('#btnTwo')
 
-let num = 0;
-let timeRefrence;
-btn.addEventListener('click',()=>{
-    p.innerHTML =`Loading data........`  
-    timeRefrence = setInterval(() => {
-        p.innerHTML =`hy your lucky number is it-> ${num} `   
-        num++;
-    }, 1000);
-});
+// let num = 0;
+// let timeRefrence;
+// btn.addEventListener('click',()=>{
+//     p.innerHTML =`Loading data........`  
+//     timeRefrence = setInterval(() => {
+//         p.innerHTML =`hy your lucky number is it-> ${num} `   
+//         num++;
+//     }, 1000);
+// });
 
-stopbtn.addEventListener('click',()=>{
-    clearInterval(timeRefrence) //stop the timer when click stopbtn  and  Clear the interval using the stored reference
-    p.innerHTML += 'Interval stopped'
-});
+
+// stopbtn.addEventListener('click',()=>{
+//     clearInterval(timeRefrence) //stop the timer when click stopbtn  and  Clear the interval using the stored reference
+//     p.innerHTML += 'Interval stopped'
+// });
 
 
 
@@ -85,3 +86,59 @@ stopbtn.addEventListener('click',()=>{
 //    clearInterval();// when you dont use the ref variable till then this will not work
 //    p.innerHTML = 'Interval stopped';
 //});
+
+
+//-----------------------
+//ye every 3 sec. me baar baar call hoga and  task print krega and setInterval() Infinite time chlta rhega jb tk usko rokege nhi
+
+// setInterval(function a(){
+//   console.log("this is setInterval and it will execute after 3 seconds")
+// },3000)
+
+
+
+// setInterval(function(){
+//     console.log("this is setInterval and it will execute every 3 seconds again and again")
+// },3000)
+
+
+//-------------------------
+// let idRef = setInterval(function(){
+//     console.log("this is setInterval and it will not  execute every 3 seconds again and again")
+// },2000)
+
+
+//  setInterval(function(){
+//      clearInterval(idRef)
+// },10000)
+//it will execute the code 5 times every 2 sec.
+
+
+//---------------------
+// let Idddd = setInterval(function(){ //imp
+//     console.log("hi i am swaggy")
+// } , 2000)
+
+
+// setTimeout(function(){
+//     clearInterval(Idddd); //imp 
+// } , 4000)
+
+//it will execute the code 2 times every 2 sec.
+
+
+//-------------------
+let Id = setInterval(function(){ //imp
+    console.log("hi i am swaggy")
+} , 1000)
+
+
+setTimeout(function(){
+    clearInterval(Id); //imp 
+} , 4000)
+
+//it will execute the code 4 times every 1 sec.
+
+
+//NOTE: setInterval returns an ID  and hme ydi function execution  ko stop krna hai to hm clearInterval ka use krege
+//and uske under is ID ko pass kr denge clearInterval(ID) jisse setInterval stop ho jayega
---
+++ js/basic/Timing Events/crousal-project/app.js
@@ -0,0 +1,36 @@
+const imgEl = document.querySelector('img')
+
+let arr = [
+    'https://wallpapercave.com/wp/wp2599594.jpg',
+    'https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?cs=srgb&dl=daylight-forest-glossy-443446.jpg&fm=jpg',
+    'https://www.thewowstyle.com/wp-content/uploads/2015/01/nature-images..jpg',
+    'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?cs=srgb&dl=daylight-environment-forest-459225.jpg&fm=jpg',
+    'https://www.pixelstalk.net/wp-content/uploads/2016/07/Wallpapers-pexels-photo.jpg'
+]
+
+let ArrIndex = 0;
+let stopId = setInterval(function(){
+    imgEl.setAttribute('src',arr[ArrIndex])
+    ArrIndex = (ArrIndex + 1) % arr.length; //modulas->reaminder
+},3000)
+
+setTimeout(function(){
+    clearInterval(stopId)
+},30000)
+// 30000 miliSeconds = 30 seconds
+
+
+//or
+
+// let i = 0;
+// setInterval(function(){
+//     imgEl.src =  arr[i]
+//     if(i < arr.length-1){ //4 < 4 then set i=0 bcz in array always index/i num start with 0 and length start with 1
+//         i++;
+//     }
+//     else{
+//         i = 0;
+//     }
+// },2000)
+
+
